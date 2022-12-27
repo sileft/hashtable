765203b7dd97cbb6e8edcf85531ea2c960e2b87d
saipc <saipc@SAIPC>
2022-12-27 12:40:12 -0600

    Agregar archivos de proyecto.

---
+++ hashtable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+             
+        /*(11.4.2.1) Crea un programa que, cuando el usuario introduzca el nombre de un
+        número del 1 al 10 en inglés (por ejemplo, "two"), diga su traducción en español
+        (por ejemplo, "dos").
+
+        (11.4.2.2) Crea un traductor básico de C# a Pascal, que tenga las traducciones
+        almacenadas en una SortedList (por ejemplo, "{" se convertirá a "begin", "}" se
+        convertirá a "begin", "WriteLine" se convertirá a "WriteLn", "ReadLine" se convertirá
+        a "ReadLn", "void" se convertirá a "procedure" y "Console." se convertirá a una
+        cadena vacía. Úsalo para convertir un abrir un fichero que contenga un fuente en
+        C# y mostrar su equivalente (que no será perfecto) en Pascal.
+
+        Hashtable slistita = new Hashtable();
+
+        slistita.Add("one", "uno");
+        slistita.Add("two", "dos");
+        slistita.Add("three", "tres");
+        slistita.Add("four", "cuatro");
+        slistita.Add("five", "cinco");
+        slistita.Add("six", "seis");
+        slistita.Add("seven", "siete");
+        slistita.Add("eight", "ocho");
+        slistita.Add("nine", "nueve");
+        slistita.Add("ten", "diez");
+        string numero;
+
+        Console.WriteLine("Escribe el numero");
+        numero=Console.ReadLine();
+        Console.WriteLine("La traducción al español es "+(slistita[numero]));*/
+
+
+
+    }
+}
